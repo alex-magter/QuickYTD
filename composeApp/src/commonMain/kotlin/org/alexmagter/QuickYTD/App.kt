3e9b80c577cc7e67b80bfb2489945cce7769baca
AlexelNintendero <alextrueno3@gmail.com>
2025-07-17 15:52:16 +0200

    Added app title to main screen

--- composeApp/src/commonMain/kotlin/org/alexmagter/QuickYTD/App.kt
+++ composeApp/src/commonMain/kotlin/org/alexmagter/QuickYTD/App.kt
@@ -13,7 +13,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -30,9 +33,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -71,6 +79,31 @@ fun App(navController: NavController, viewModel: SharedViewModel) {
                 .fillMaxSize()
                 .background(DarkTheme.backgroundColor)
         ) {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5f), // ocupa la mitad de la altura
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        append("Quick")
+                        withStyle(style = SpanStyle(color = Color.Red)) {
+                            append("YT") // letra "E" en azul y negrita
+                        }
+                        withStyle(style = SpanStyle(color = Color.Blue)) {
+                            append("D") // letra "E" en azul y negrita
+                        }
+
+                    },
+                    color = Color.White,
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+            }
+
             Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
@@ -123,7 +156,7 @@ fun App(navController: NavController, viewModel: SharedViewModel) {
                     enabled = !isGettingData,
                     colors = DarkTheme.SearcbButtonColors(!isGettingData)
                 ){
-                    Text("Buscar")
+                    Text("Search")
                 }
 
 
