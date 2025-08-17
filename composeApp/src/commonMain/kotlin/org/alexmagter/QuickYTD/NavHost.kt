0bf733410fe3b2f010fa9d0e647f93b62a77b9c4
AlexelNintendero <alextrueno3@gmail.com>
2025-08-18 01:01:23 +0200

    Changed navhost animation
    Now the animation between screens is a slide over animation, more modern and smooth

--- composeApp/src/commonMain/kotlin/org/alexmagter/QuickYTD/NavHost.kt
+++ composeApp/src/commonMain/kotlin/org/alexmagter/QuickYTD/NavHost.kt
@@ -1,9 +1,17 @@
 package org.alexmagter.QuickYTD
 
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -21,44 +29,56 @@ enum class SearchScreen() {
 fun Navigation(fileSaver: FileSaver){
     val navController = rememberNavController()
     val sharedViewModel: SharedViewModel = viewModel()
-    val animationDurationMillis = 500
+    val animationDurationMillis = 300
+    val slideUnderPercentage = 0.2
+    val fadePercentage = 0.2f
 
-    NavHost(
-        navController = navController,
-        startDestination = SearchScreen.Start.name,
-        enterTransition = {
+    Box(Modifier.fillMaxSize().background(Color.Black)) {
+        NavHost(
+            navController = navController,
+            startDestination = SearchScreen.Start.name,
+            enterTransition = {
 
-            slideInHorizontally(
-                initialOffsetX = { fullWidth -> fullWidth }, // Entra desde la derecha (fuera de la pantalla a la derecha)
-                animationSpec = tween(durationMillis = animationDurationMillis)
-            )
-        },
-        exitTransition = {
-            slideOutHorizontally(
-                targetOffsetX = { fullWidth -> -fullWidth }, // Sale hacia la izquierda (fuera de la pantalla a la izquierda)
-                animationSpec = tween(durationMillis = animationDurationMillis)
-            )
-        },
-        popEnterTransition = {
-            slideInHorizontally(
-                initialOffsetX = { fullWidth -> -fullWidth }, // Entra desde la izquierda
-                animationSpec = tween(durationMillis = animationDurationMillis)
-            )
-        },
-        popExitTransition = {
-            slideOutHorizontally(
-                targetOffsetX = { fullWidth -> fullWidth }, // Sale hacia la derecha
-                animationSpec = tween(durationMillis = animationDurationMillis)
-            )
-        },
+                slideInHorizontally(
+                    initialOffsetX = { fullWidth -> fullWidth }, // Entra desde la derecha (fuera de la pantalla a la derecha)
+                    animationSpec = tween(durationMillis = animationDurationMillis)
+                )
+            },
+            exitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { fullWidth -> (-fullWidth*slideUnderPercentage).toInt() }, // Sale hacia la izquierda (fuera de la pantalla a la izquierda)
+                    animationSpec = tween(durationMillis = animationDurationMillis)
+                ) + fadeOut(
+                    targetAlpha = fadePercentage,
+                    animationSpec = tween(durationMillis = animationDurationMillis)
+                )
+            },
+            popEnterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { fullWidth -> -(fullWidth*slideUnderPercentage).toInt() }, // Entra desde la izquierda
+                    animationSpec = tween(durationMillis = animationDurationMillis)
+                ) + fadeIn(
+                    initialAlpha = fadePercentage,
+                    animationSpec = tween(durationMillis = animationDurationMillis)
+                )
+            },
+            popExitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { fullWidth -> fullWidth }, // Sale hacia la derecha
+                    animationSpec = tween(durationMillis = animationDurationMillis)
+                )
+            },
 
-    ) {
-        composable(route = SearchScreen.Start.name) {
-            App(navController, sharedViewModel)
-        }
+            ) {
+            composable(route = SearchScreen.Start.name) {
+                App(navController, sharedViewModel)
+            }
 
-        composable(route = SearchScreen.VideoPage.name) {
-            VideoPage(navController, sharedViewModel, fileSaver)
+            composable(route = SearchScreen.VideoPage.name) {
+                VideoPage(navController, sharedViewModel, fileSaver)
+            }
         }
     }
+
+
 }
